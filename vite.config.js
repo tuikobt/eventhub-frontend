9e6ea010e2305a5b3da99b0b38eb28d5d886603b
tuikobt <leminhphuong15032005@gmail.com>
2026-06-30 00:15:40 +0700

    fix: config tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/postcss";
 
 // https://vite.dev/config/
 export default defineConfig({
